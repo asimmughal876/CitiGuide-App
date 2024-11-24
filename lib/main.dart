eac225933c97ea4dbe3d95f199529b11802777e2
simra098 <121328623+simra098@users.noreply.github.com>
2024-11-24 08:07:07 -0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:citi_guide_app/home.dart';
+//import 'package:citi_guide_app/home.dart';
+//import 'package:citi_guide_app/Aboutus.dart';
+import 'package:citi_guide_app/admin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +13,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  Scaffold(
-          appBar: AppBar(
-          title: const Text("Citi Guide" ,style: TextStyle(color: Colors.white),),
-          backgroundColor: const Color.fromRGBO(244, 65, 83, 1), // AppBar background color
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Citi Guide",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: const Color.fromARGB(
+              255, 185, 233, 255), // AppBar background color
         ),
 
-        body: const HomePage(),
+        //body: const HomePage(),
+        //body: const Aboutus(),
+        body: const AdminDashboard(),
       ),
     );
   }
