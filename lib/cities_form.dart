31a051418e4da957dbb204d88d268a8306a2dfc9
asimmughal876 <opasim876@gmail.com>
2024-11-27 17:37:00 +0500

    added city form attraction form

---
+++ lib/cities_form.dart
@@ -0,0 +1,265 @@
+import 'dart:io';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CitiesForm extends StatefulWidget {
+  const CitiesForm({super.key});
+
+  @override
+  State<CitiesForm> createState() => _CitiesFormState();
+}
+
+class _CitiesFormState extends State<CitiesForm> {
+  final cityController = TextEditingController();
+  final citydesc = TextEditingController();
+  final DatabaseReference citiesRef =
+      FirebaseDatabase.instance.ref().child('cities');
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+  String? _imageUrl;
+  bool _isUploading = false;
+
+  final cloudinary = CloudinaryPublic(
+    'djhjm9vtp', // Replace with your cloud name
+    'images', // Replace with your upload preset
+    cache: false,
+  );
+  // Pick an image and convert it to Base64
+  Future<void> _pickImage(ImageSource source) async {
+    try {
+      final XFile? pickedFile = await _picker.pickImage(
+        source: source,
+        maxWidth: 1080,
+        maxHeight: 1080,
+        imageQuality: 85,
+      );
+
+      if (pickedFile != null) {
+        setState(() {
+          _image = pickedFile;
+        });
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  Future<String?> _uploadToCloudinary() async {
+    if (_image == null) return null;
+
+    try {
+      CloudinaryResponse response;
+
+      if (kIsWeb) {
+        final bytes = await _image!.readAsBytes();
+        response = await cloudinary.uploadFile(
+          CloudinaryFile.fromBytesData(
+            bytes,
+            identifier: 'upload',
+            folder: 'cities',
+            resourceType: CloudinaryResourceType.Image,
+          ),
+        );
+      } else {
+        response = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(
+            _image!.path,
+            folder: 'cities',
+            resourceType: CloudinaryResourceType.Image,
+          ),
+        );
+      }
+
+      return response.secureUrl;
+    } catch (e) {
+      print('Error uploading to Cloudinary: $e');
+      return null;
+    }
+  }
+
+  Future<void> addCity() async {
+    if (cityController.text.isEmpty || _image == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please complete all fields and select an image'),
+        ),
+      );
+      return;
+    }
+
+    setState(() {
+      _isUploading = true;
+    });
+
+    try {
+      _imageUrl = await _uploadToCloudinary();
+
+      if (_imageUrl == null) throw Exception('Failed to upload image');
+
+      await citiesRef.push().set({
+        'city': cityController.text,
+        'image': _imageUrl,
+        'description': citydesc.text,
+      });
+      cityController.clear();
+      citydesc.clear();
+
+      setState(() {
+        _image = null;
+        _imageUrl = null;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('City added successfully')),
+      );
+    } on FirebaseException catch (e) {
+      setState(() {
+        _isUploading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Failed to add city: ${e.message}')),
+      );
+    } catch (e) {
+      setState(() {
+        _isUploading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Unknown error: $e')),
+      );
+    } finally {
+      setState(() {
+        _isUploading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Text(
+                "City Form",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: cityController,
+                cursorColor:
+                    const Color.fromRGBO(244, 65, 83, 1), // Set cursor color
+                decoration: InputDecoration(
+                    labelText: "City Name",
+                    hintText: "Enter city name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.black, // Active border color
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelStyle: TextStyle(color: Colors.grey[900])),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () => _pickImage(ImageSource.gallery),
+                      icon: const Icon(Icons.photo_library),
+                      label: const Text("Choose Image"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromRGBO(244, 65, 83, 1),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              if (_image != null)
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  width: double.infinity,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: kIsWeb
+                        ? Image.network(
+                            _image!.path,
+                            fit: BoxFit.cover,
+                          )
+                        : Image.file(
+                            File(_image!.path),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ),
+              const SizedBox(height: 16),
+              TextField(
+                controller: citydesc,
+                minLines: 5,
+                maxLines: 7,
+                cursorColor:
+                    const Color.fromRGBO(244, 65, 83, 1), // Set cursor color
+                decoration: InputDecoration(
+                    labelText: "City Description",
+                    hintText: "Enter city Description",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: Colors.black, // Active border color
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelStyle: TextStyle(color: Colors.grey[900])),
+              ),
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: _isUploading ? null : addCity,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(244, 65, 83, 1),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                ),
+                child: _isUploading
+                    ? const CircularProgressIndicator(color: Colors.white)
+                    : const Text(
+                        "Submit",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
