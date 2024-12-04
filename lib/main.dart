33be2ed16d11c6f038f13672ebf571103568830a
asimmughal876 <opasim876@gmail.com>
2024-12-04 15:10:01 +0500

    added a splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
 import 'package:citi_guide_app/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-       home: BottomNav(),
-      
+       home: BottomNav(),      
     );
   }
 }
